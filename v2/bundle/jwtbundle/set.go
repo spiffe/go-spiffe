69d209272df2d0de6d4ec80e631492d8c6a5bfa5
Agustín Martínez Fayó <amartinezfayo@users.noreply.github.com>
2020-03-18 19:13:34 -0300

    Implement jwtbundle.Set type (#78)
    Signed-off-by: Agustín Martínez Fayó <amartinezfayo@gmail.com>

--- v2/bundle/jwtbundle/set.go
+++ v2/bundle/jwtbundle/set.go
@@ -1,35 +1,69 @@
 package jwtbundle
 
 import (
+	"sync"
+
 	"github.com/spiffe/go-spiffe/v2/spiffeid"
 )
 
 // Set is a set of bundles, keyed by trust domain.
-type Set struct{}
+type Set struct {
+	mtx     sync.RWMutex
+	bundles map[spiffeid.TrustDomain]*Bundle
+}
 
-// NewSet creates a new, empty set.
+// NewSet creates a new set initialized with the given bundles
 func NewSet(bundles ...*Bundle) *Set {
-	panic("not implemented")
+	bundlesMap := make(map[spiffeid.TrustDomain]*Bundle)
+
+	for _, b := range bundles {
+		if b != nil {
+			bundlesMap[b.trustDomain] = b
+		}
+	}
+
+	return &Set{
+		bundles: bundlesMap,
+	}
 }
 
 // Add adds a new bundle into the set. If a bundle already exists for the
 // trust domain, the existing bundle is replaced.
 func (s *Set) Add(bundle *Bundle) {
-	panic("not implemented")
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+
+	if bundle != nil {
+		s.bundles[bundle.trustDomain] = bundle
+	}
 }
 
 // Remove removes the bundle for the given trust domain.
 func (s *Set) Remove(trustDomain spiffeid.TrustDomain) {
-	panic("not implemented")
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+
+	delete(s.bundles, trustDomain)
 }
 
 // Has returns true if there is a bundle for the given trust domain.
 func (s *Set) Has(trustDomain spiffeid.TrustDomain) bool {
-	panic("not implemented")
+	s.mtx.RLock()
+	defer s.mtx.RUnlock()
+
+	_, ok := s.bundles[trustDomain]
+	return ok
 }
 
 // GetJWTBundleForTrustDomain returns the JWT bundle of the given trust domain.
 // It implements the Source interface.
 func (s *Set) GetJWTBundleForTrustDomain(trustDomain spiffeid.TrustDomain) (*Bundle, error) {
-	panic("not implemented")
+	s.mtx.RLock()
+	defer s.mtx.RUnlock()
+
+	if !s.Has(trustDomain) {
+		return nil, jwtbundleErr.New("no JWT bundle for trust domain %q", trustDomain)
+	}
+
+	return s.bundles[trustDomain], nil
 }
