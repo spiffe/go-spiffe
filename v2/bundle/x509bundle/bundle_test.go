1a8e194495788c1ccc23aa5f8c2dae97b7ac22b2
Marcos G. Yedro <marcosyedro@gmail.com>
2020-03-18 10:19:42 -0300

    Add trust-domain changes required after merge
    Signed-off-by: Marcos G. Yedro <marcosyedro@gmail.com>

--- v2/bundle/x509bundle/bundle_test.go
+++ v2/bundle/x509bundle/bundle_test.go
@@ -11,22 +11,22 @@ import (
 )
 
 func TestNew(t *testing.T) {
-	bundle := New("example.org")
+	bundle := New(spiffeid.RequireTrustDomainFromString("example.org"))
 	require.NotNil(t, bundle)
 	require.NotNil(t, bundle.X509Roots())
 	assert.Len(t, bundle.X509Roots(), 0)
-	assert.Equal(t, spiffeid.TrustDomain("example.org"), bundle.TrustDomain())
+	assert.Equal(t, spiffeid.RequireTrustDomainFromString("example.org"), bundle.TrustDomain())
 }
 
 func TestLoad_Succeeds(t *testing.T) {
-	bundle, err := Load("example.org", "testdata/certs.pem")
+	bundle, err := Load(spiffeid.RequireTrustDomainFromString("example.org"), "testdata/certs.pem")
 	require.NoError(t, err)
 	require.NotNil(t, bundle)
 	assert.Len(t, bundle.X509Roots(), 2)
 }
 
 func TestLoad_Fails(t *testing.T) {
-	bundle, err := Load("example.org", "testdata/non-existent-file.pem")
+	bundle, err := Load(spiffeid.RequireTrustDomainFromString("example.org"), "testdata/non-existent-file.pem")
 	require.Error(t, err)
 	require.Contains(t, err.Error(), "unable to load X.509 bundle file")
 	assert.Nil(t, bundle)
@@ -37,7 +37,7 @@ func TestRead_Succeeds(t *testing.T) {
 	require.NoError(t, err)
 	defer file.Close()
 
-	bundle, err := Read("example.org", file)
+	bundle, err := Read(spiffeid.RequireTrustDomainFromString("example.org"), file)
 	require.NoError(t, err)
 	require.NotNil(t, bundle)
 	assert.Len(t, bundle.X509Roots(), 2)
@@ -50,7 +50,7 @@ func TestRead_Fails(t *testing.T) {
 	// Close file prematurely to cause an error while reading
 	file.Close()
 
-	bundle, err := Read("example.org", file)
+	bundle, err := Read(spiffeid.RequireTrustDomainFromString("example.org"), file)
 	require.Error(t, err)
 	require.Contains(t, err.Error(), "unable to read")
 	assert.Nil(t, bundle)
@@ -102,7 +102,7 @@ func TestParse(t *testing.T) {
 			fileBytes, err := ioutil.ReadFile(test.path)
 			require.NoError(t, err)
 
-			bundle, err := Parse("example.org", fileBytes)
+			bundle, err := Parse(spiffeid.RequireTrustDomainFromString("example.org"), fileBytes)
 			if test.expErrContains != "" {
 				require.Error(t, err)
 				assert.Contains(t, err.Error(), test.expErrContains)
@@ -117,13 +117,13 @@ func TestParse(t *testing.T) {
 
 func TestX509RootCRUD(t *testing.T) {
 	// Load bundle1, which contains a single certificate
-	bundle1, err := Load("example-1.org", "testdata/cert.pem")
+	bundle1, err := Load(spiffeid.RequireTrustDomainFromString("example-1.org"), "testdata/cert.pem")
 	require.NoError(t, err)
 	assert.Len(t, bundle1.X509Roots(), 1)
 
 	// Load bundle2, which contains 2 certificates
 	// The first certificate is the same than the one used in bundle1
-	bundle2, err := Load("example-2.org", "testdata/certs.pem")
+	bundle2, err := Load(spiffeid.RequireTrustDomainFromString("example-2.org"), "testdata/certs.pem")
 	require.NoError(t, err)
 	assert.Len(t, bundle2.X509Roots(), 2)
 	assert.True(t, bundle2.HasX509Root(bundle1.X509Roots()[0]))
@@ -155,7 +155,7 @@ func TestX509RootCRUD(t *testing.T) {
 
 func TestMarshal(t *testing.T) {
 	// Load a bundle to marshal
-	bundle, err := Load("example.org", "testdata/certs.pem")
+	bundle, err := Load(spiffeid.RequireTrustDomainFromString("example.org"), "testdata/certs.pem")
 	require.NoError(t, err)
 
 	// Marshal the bundle
@@ -172,20 +172,20 @@ func TestMarshal(t *testing.T) {
 }
 
 func TestGetX509BundleForTrustDomain_Succeeds(t *testing.T) {
-	bundle, err := Load("example.org", "testdata/certs.pem")
+	bundle, err := Load(spiffeid.RequireTrustDomainFromString("example.org"), "testdata/certs.pem")
 	require.NoError(t, err)
 
-	b, err := bundle.GetX509BundleForTrustDomain("example.org")
+	b, err := bundle.GetX509BundleForTrustDomain(spiffeid.RequireTrustDomainFromString("example.org"))
 	require.NoError(t, err)
 	require.NotNil(t, b)
 	require.Equal(t, bundle, b)
 }
 
 func TestGetX509BundleForTrustDomain_Fails(t *testing.T) {
-	bundle, err := Load("example.org", "testdata/certs.pem")
+	bundle, err := Load(spiffeid.RequireTrustDomainFromString("example.org"), "testdata/certs.pem")
 	require.NoError(t, err)
 
-	b, err := bundle.GetX509BundleForTrustDomain("another-td.org")
+	b, err := bundle.GetX509BundleForTrustDomain(spiffeid.RequireTrustDomainFromString("another-td.org"))
 	require.Error(t, err)
 	require.Contains(t, err.Error(), `no X.509 bundle found for trust domain: "another-td.org"`)
 	require.Nil(t, b)
