7ec9f0e63fd4c9a3a84383531becfe2b6a3ebfda
Andrew Harding <azdagron@gmail.com>
2020-03-24 13:41:07 -0600

    Add authorizers to spiffetls convenience functions
    Signed-off-by: Andrew Harding <azdagron@gmail.com>

--- v2/spiffetls/examples_test.go
+++ v2/spiffetls/examples_test.go
@@ -4,11 +4,15 @@ import (
 	"context"
 	"crypto/tls"
 
+	"github.com/spiffe/go-spiffe/v2/spiffeid"
 	"github.com/spiffe/go-spiffe/v2/spiffetls"
+	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
 )
 
 func ExampleListenMTLS() {
-	listener, err := spiffetls.ListenMTLS(context.TODO(), "tcp", ":8443")
+	td := spiffeid.RequireTrustDomainFromString("example.org")
+
+	listener, err := spiffetls.ListenMTLS(context.TODO(), "tcp", ":8443", tlsconfig.AuthorizeMemberOf(td))
 	if err != nil {
 		// TODO: error handling
 	}
@@ -16,12 +20,14 @@ func ExampleListenMTLS() {
 }
 
 func ExampleListenMTLS_customTLSConfigBase() {
+	td := spiffeid.RequireTrustDomainFromString("example.org")
+
 	baseConfig := &tls.Config{
 		// TODO: set up custom configuration. Note that the spiffetls package
 		// will override certificate and verification related fields.
 	}
 
-	listener, err := spiffetls.ListenMTLS(context.TODO(), "tcp", ":8443", spiffetls.WithTLSConfigBase(baseConfig))
+	listener, err := spiffetls.ListenMTLS(context.TODO(), "tcp", ":8443", tlsconfig.AuthorizeMemberOf(td), spiffetls.WithTLSConfigBase(baseConfig))
 	if err != nil {
 		// TODO: error handling
 	}
