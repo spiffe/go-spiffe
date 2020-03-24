70200bf596bc5f4ead6bbb5237e76e7dcbafeea0
Andrew Harding <azdagron@gmail.com>
2020-03-24 13:41:07 -0600

    Add authorizers to spiffetls convenience functions
    Signed-off-by: Andrew Harding <azdagron@gmail.com>

--- v2/spiffetls/listen.go
+++ v2/spiffetls/listen.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"crypto/tls"
 	"net"
+
+	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
 )
 
 // ListenTLS creates a TLS listener accepting connections on the given network
@@ -22,31 +24,35 @@ func NewTLSListener(ctx context.Context, inner net.Listener, options ...ListenOp
 
 // ListenMTLS creates an mTLS listener accepting connections on the given network
 // address using net.Listen. The server X509-SVID is obtained via the Workload
-// API along with X.509 bundles used to verify client X509-SVIDs.
-func ListenMTLS(ctx context.Context, network, laddr string, options ...ListenOption) (net.Listener, error) {
+// API along with X.509 bundles used to verify client X509-SVIDs. The client
+// is authorized using the given authorizer.
+func ListenMTLS(ctx context.Context, network, laddr string, authorizer tlsconfig.Authorizer, options ...ListenOption) (net.Listener, error) {
 	panic("not implemented")
 }
 
 // NewMTLSListener creates an mTLS listener which accepts connections from an
 // inner Listener and wraps each connection with tls.Server. The server
 // X509-SVID is obtained via the Workload API along with X.509 bundles used to
-// verify client X509-SVIDs.
-func NewMTLSListener(ctx context.Context, inner net.Listener, options ...ListenOption) (net.Listener, error) {
+// verify client X509-SVIDs. The client is authorized using the given
+// authorizer.
+func NewMTLSListener(ctx context.Context, inner net.Listener, authorizer tlsconfig.Authorizer, options ...ListenOption) (net.Listener, error) {
 	panic("not implemented")
 }
 
 // ListenWebMTLS creates an mTLS listener accepting connections on the given
 // network address using net.Listen. The certificate callback is used to
 // obtain the server certificate. Client certificates are authenticated using
-// X.509 bundles obtained via the Workload API.
-func ListenWebMTLS(ctx context.Context, network, laddr string, getCertificate func(*tls.ClientHelloInfo) (*tls.Certificate, error), options ...ListenOption) (net.Listener, error) {
+// X.509 bundles obtained via the Workload API. The client is authorized using
+// the given authorizer.
+func ListenWebMTLS(ctx context.Context, network, laddr string, getCertificate func(*tls.ClientHelloInfo) (*tls.Certificate, error), authorizer tlsconfig.Authorizer, options ...ListenOption) (net.Listener, error) {
 	panic("not implemented")
 }
 
 // NewWebMTLSListener creates an mTLS listener which accepts connections from
 // an inner Listener and wraps each connection with tls.Server. The certificate
 // callback to obtain the server certificate. Client certificates are
-// authenticated using X.509 bundles obtained via the Workload API.
-func NewWebMTLSListener(ctx context.Context, inner net.Listener, getCertificate func(*tls.ClientHelloInfo) (*tls.Certificate, error), options ...ListenOption) (net.Listener, error) {
+// authenticated using X.509 bundles obtained via the Workload API. The client
+// is authorized using the given authorizer.
+func NewWebMTLSListener(ctx context.Context, inner net.Listener, getCertificate func(*tls.ClientHelloInfo) (*tls.Certificate, error), authorizer tlsconfig.Authorizer, options ...ListenOption) (net.Listener, error) {
 	panic("not implemented")
 }
