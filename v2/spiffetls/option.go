d244685799f3ecfb22093442888655b948ee51b6
Marcos G. Yedro <marcosyedro@gmail.com>
2020-04-17 17:25:52 -0300

    Implement spiffetls dialing
    Signed-off-by: Marcos G. Yedro <marcosyedro@gmail.com>

--- v2/spiffetls/option.go
+++ v2/spiffetls/option.go
@@ -3,22 +3,42 @@ package spiffetls
 import (
 	"crypto/tls"
 	"net"
+
+	"github.com/zeebo/errs"
 )
 
+var spiffetlsErr = errs.Class("spiffetls")
+
 // DialOption is an option for dialing. Option's are also DialOption's.
-type DialOption interface{}
+type DialOption interface {
+	apply(*dialOption)
+}
+
+type dialOption struct {
+	baseTLSConf *tls.Config
+	dialer      *net.Dialer
+}
+
+func (o *dialOption) apply(other *dialOption) {
+	if other.baseTLSConf == nil {
+		other.baseTLSConf = o.baseTLSConf
+	}
+	if other.dialer == nil {
+		other.dialer = o.dialer
+	}
+}
 
 // WithDialTLSConfigBase provides a base TLS configuration to use. Fields
 // related to certificates and verification will be overwritten by this package
 // as necessary to facilitate SPIFFE authentication.
 func WithDialTLSConfigBase(base *tls.Config) DialOption {
-	panic("not implemented")
+	return &dialOption{baseTLSConf: base}
 }
 
 // WithDialer provides a net dialer to use. If unset, the standard net dialer
 // will be used.
 func WithDialer(dialer *net.Dialer) DialOption {
-	panic("not implemented")
+	return &dialOption{dialer: dialer}
 }
 
 // ListenOption is an option for listening. Option's are also ListenOption's.
