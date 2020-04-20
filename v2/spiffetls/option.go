34c6ea448d797af1e388dde5398a6944b6b988b3
Marcos Yedro <marcosyedro@gmail.com>
2020-04-20 18:04:17 -0300

    Implement spiffetls dialing (#106)
    * Implement spiffetls dialing
    
    Signed-off-by: Marcos G. Yedro <marcosyedro@gmail.com>
    
    * Address PR comments
    
    Signed-off-by: Marcos G. Yedro <marcosyedro@gmail.com>
    
    * Removing extra '*'
    
    Signed-off-by: Marcos G. Yedro <marcosyedro@gmail.com>
    
    * Address PR comments
    
    Signed-off-by: Marcos G. Yedro <marcosyedro@gmail.com>

--- v2/spiffetls/option.go
+++ v2/spiffetls/option.go
@@ -3,22 +3,43 @@ package spiffetls
 import (
 	"crypto/tls"
 	"net"
+
+	"github.com/zeebo/errs"
 )
 
+var spiffetlsErr = errs.Class("spiffetls")
+
 // DialOption is an option for dialing. Option's are also DialOption's.
-type DialOption interface{}
+type DialOption interface {
+	apply(*dialConfig)
+}
+
+type dialOption func(*dialConfig)
+
+func (fn dialOption) apply(c *dialConfig) {
+	fn(c)
+}
+
+type dialConfig struct {
+	baseTLSConf *tls.Config
+	dialer      *net.Dialer
+}
 
 // WithDialTLSConfigBase provides a base TLS configuration to use. Fields
 // related to certificates and verification will be overwritten by this package
 // as necessary to facilitate SPIFFE authentication.
 func WithDialTLSConfigBase(base *tls.Config) DialOption {
-	panic("not implemented")
+	return dialOption(func(c *dialConfig) {
+		c.baseTLSConf = base
+	})
 }
 
 // WithDialer provides a net dialer to use. If unset, the standard net dialer
 // will be used.
 func WithDialer(dialer *net.Dialer) DialOption {
-	panic("not implemented")
+	return dialOption(func(c *dialConfig) {
+		c.dialer = dialer
+	})
 }
 
 // ListenOption is an option for listening. Option's are also ListenOption's.
