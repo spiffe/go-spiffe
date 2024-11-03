06cd3ca9c14eea1a1627a910f49f7de69a5ca566
junya koyama <arukiidou@yahoo.co.jp>
2024-11-04 01:34:01 +0900

    remove errs.Class("federation")
    Signed-off-by: junya koyama <arukiidou@yahoo.co.jp>

--- v2/federation/watch.go
+++ v2/federation/watch.go
@@ -2,6 +2,7 @@ package federation
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/spiffe/go-spiffe/v2/bundle/spiffebundle"
@@ -36,7 +37,7 @@ type BundleWatcher interface {
 // context is canceled, returning ctx.Err().
 func WatchBundle(ctx context.Context, trustDomain spiffeid.TrustDomain, url string, watcher BundleWatcher, options ...FetchOption) error {
 	if watcher == nil {
-		return federationErr.New("watcher cannot be nil")
+		return wrapFederationErr(errors.New("watcher cannot be nil"))
 	}
 
 	latestBundle := &spiffebundle.Bundle{}
