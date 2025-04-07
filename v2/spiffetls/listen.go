3d122ae7eadf0e3c68e6905b9553db13587e1ace
Andrew Harding <azdagron@gmail.com>
2025-04-07 08:02:36 -0600

    fix lint
    Signed-off-by: Andrew Harding <azdagron@gmail.com>

--- v2/spiffetls/listen.go
+++ v2/spiffetls/listen.go
@@ -147,5 +147,5 @@ type serverConn struct {
 // been completed. Note that in Go's TLS stack, the TLS 1.3 handshake may not
 // complete until the first read from the connection.
 func (c *serverConn) PeerID() (spiffeid.ID, error) {
-	return PeerIDFromConnectionState(c.Conn.ConnectionState())
+	return PeerIDFromConnectionState(c.ConnectionState())
 }
