696a8719cd4123e1a7b2739c08e7051a0615ed99
Jason Costello <jason@cofide.io>
2025-10-10 22:26:07 +0100

    Remaining s/jwtbundle/witbundle fixes
    Signed-off-by: Jason Costello <jason@cofide.io>

--- bundle/witbundle/set_test.go
+++ bundle/witbundle/set_test.go
@@ -3,34 +3,34 @@ package witbundle_test
 import (
 	"testing"
 
-	"github.com/spiffe/go-spiffe/v2/bundle/jwtbundle"
+	"github.com/spiffe/go-spiffe/v2/bundle/witbundle"
 	"github.com/spiffe/go-spiffe/v2/spiffeid"
 	"github.com/stretchr/testify/require"
 )
 
 var (
-	b1  = jwtbundle.New(td)
+	b1  = witbundle.New(td)
 	td2 = spiffeid.RequireTrustDomainFromString("example-2.org")
 )
 
 func TestNewSet(t *testing.T) {
-	s := jwtbundle.NewSet(b1)
+	s := witbundle.NewSet(b1)
 	require.True(t, s.Has(td))
 
-	s = jwtbundle.NewSet(jwtbundle.New(td), jwtbundle.New(td2))
+	s = witbundle.NewSet(witbundle.New(td), witbundle.New(td2))
 	require.True(t, s.Has(td))
 	require.True(t, s.Has(td2))
 }
 
 func TestAdd(t *testing.T) {
-	s := jwtbundle.NewSet()
+	s := witbundle.NewSet()
 	require.False(t, s.Has(td))
 	s.Add(b1)
 	require.True(t, s.Has(td))
 }
 
 func TestRemove(t *testing.T) {
-	s := jwtbundle.NewSet(b1)
+	s := witbundle.NewSet(b1)
 	require.True(t, s.Has(td))
 	s.Remove(td2)
 	require.True(t, s.Has(td))
@@ -39,17 +39,17 @@ func TestRemove(t *testing.T) {
 }
 
 func TestHas(t *testing.T) {
-	s := jwtbundle.NewSet(jwtbundle.New(td))
+	s := witbundle.NewSet(witbundle.New(td))
 	require.False(t, s.Has(td2))
 	require.True(t, s.Has(td))
 }
 
 func TestSetGetJWTBundleForTrustDomain(t *testing.T) {
-	s := jwtbundle.NewSet(b1)
-	_, err := s.GetJWTBundleForTrustDomain(td2)
-	require.EqualError(t, err, `jwtbundle: no JWT bundle for trust domain "example-2.org"`)
+	s := witbundle.NewSet(b1)
+	_, err := s.GetWITBundleForTrustDomain(td2)
+	require.EqualError(t, err, `witbundle: no JWT bundle for trust domain "example-2.org"`)
 
-	b, err := s.GetJWTBundleForTrustDomain(td)
+	b, err := s.GetWITBundleForTrustDomain(td)
 	require.NoError(t, err)
 	require.Equal(t, b1, b)
 }
