20e33c7a9eede82e106c578ebb9ca72ac2317da0
Jason Costello <jason@cofide.io>
2025-10-28 20:57:38 +0000

    [From review] Adds godoc for new exp package
    Signed-off-by: Jason Costello <jason@cofide.io>

---
+++ exp/doc.go
@@ -0,0 +1,6 @@
+// Package exp contains experimental implementations of go-spiffe components
+// * witbundle - Bundle support for WIT-SVIDs
+//
+// Note: Code herein is may be subject to breaking changes, and should not be
+// considered stable
+package exp
