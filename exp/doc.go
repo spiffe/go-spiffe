f685b2d52314a6205367ef3b29981995d4e60a4a
Matheus Pimenta <matheuscscp@gmail.com>
2026-06-16 12:49:06 +0100

    Add Broker API (#388)
    Supersedes: https://github.com/spiffe/spire-api-sdk/pull/92
    https://github.com/spiffe/spire-api-sdk/pull/96
    
    SPIFFE PRs: https://github.com/spiffe/spiffe/pull/340
    https://github.com/arndt-s/spiffe/pull/1
    SPIRE PR: https://github.com/spiffe/spire/pull/6915
    
    Hey @arndt-s, @sorindumitru suggested adding this directly here instead
    of `spire-api-sdk` since this comes directly from SPIFFE and not SPIRE.
    
    Signed-off-by: Matheus Pimenta <matheuscscp@gmail.com>

---
+++ exp/doc.go
@@ -0,0 +1,6 @@
+// Package exp contains experimental implementations of go-spiffe components
+// * proto/spiffe/broker - SPIFFE Broker API protobuf definitions
+//
+// Note: Code herein may be subject to breaking changes, and should not be
+// considered stable
+package exp
